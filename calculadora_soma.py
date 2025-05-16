870969c8536aa52e146c470d8a85e5972cc0ff0d
Giovanna <giovannadelnascimento@gmail.com>
2025-05-16 20:50:06 -0300

    Feat: Conteúdo Parte01

---
+++ calculadora_soma.py
@@ -0,0 +1,12 @@
+# calcular dois numeros
+
+numero1 = float(input("Digite o primeiro número:"))
+numero2 = float(input("Digite o segundo número:"))
+
+soma = numero1 + numero2
+
+print(f"A soma do {numero1} e do {numero2} é {soma}")
+
+
+
+
