cdd6843ba2f9901d7d3705b06bf6ae263e77de28
Giovanna <giovannadelnascimento@gmail.com>
2025-05-19 22:19:02 -0300

    Feat: Conteúdo Aula03

---
+++ exercicios/calcula_cadeira.py
@@ -0,0 +1,15 @@
+# Esse código tem como objetivo calcular o preço total de um produto
+
+# Var com detalhes do produto
+nome_produto = "Cadeira de Balanço"
+preco_unitario = 20.60
+quantidade = 5
+
+# Var de cálulo do valor total
+preco_total = quantidade * preco_unitario
+
+# Print com o resultado final
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {preco_total:.2f}")
---
+++ exercicios/calcula_volume_caixa.py
@@ -0,0 +1,9 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+volume = comprimento * largura * altura
+
+print(f"o volume da caixa é : {volume} cm ²")
+                               
+
